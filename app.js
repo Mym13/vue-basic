5a14b8bfb876a71ebe10843a0a312f2c1df377e1
mym <mym9413@outlook.com>
2019-07-13 14:13:43 +0800

    vue的v-for指令[C

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ new Vue({
     el: '#vue-app',
     data: {
         characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+        // 下面这种结构，[]代表数组，{}代表对象
         ninjas: [
             { name: 'Ryu', age: 25 },
             { name: 'Yoshi', age: 35 },
