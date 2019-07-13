eda8a13c15ce701faeea102e0e0f0a93406ed3d2
mym <mym9413@outlook.com>
2019-07-13 19:12:45 +0800

    vue注册组件(全局-局部)

--- src/App.vue
+++ src/App.vue
@@ -2,27 +2,32 @@
 <template>
   <!-- template下有且仅有一个根标签 -->
   <div id="app">
-    {{title}}
+    <h1>{{title}}</h1>
+    <users></users>
   </div>
 </template>
 
 // 2.js处理逻辑
 <script>
-// 1.引入组件
+// 1).引入组件
 // import HelloWorld from "./components/HelloWorld"
+import Users from "./components/Users";
 
 export default {
-  name: 'app',
+  name: "app",
   data() {
     return {
       title: "第一个vue-cli"
-    }
+    };
   },
-  // 2.引入组件后需在此注册组件
+  // 2).引入组件后需在此注册组件
   components: {
     // HelloWorld
+    // 3).注册时，:前给定引用组件时的标签名，:后给定引入的组件名
+    // :前的标签名可省略，默认首字母小写
+    users: Users
   }
-}
+};
 </script>
 
 // 3.css样式
---
+++ src/components/Users.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="users">
+      <ul>
+          <li v-for="item in users" :key="item">{{item}}</li>
+      </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'users',
+  data () {
+    return {
+        users: ["name1","name2","name3"]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+// scoped表示下面的css样式只作用于当前这个组件
+<style scoped>
+</style>
