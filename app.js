b2b20149e7a06d8871be40e49706e700f3791806
mym <mym9413@outlook.com>
2019-07-13 15:09:23 +0800

    vue组件

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
+// vue组件
+
+//             这里是组件名，调用时使用标签形式调用<greeting></greeting>
 Vue.component('greeting', {
-    template: '<p>组件应用</p>'
+    // template中有且仅有一个根标签
+    template: 
+        `<div>
+            <p>组件应用 {{name}}</p>
+            <button @click="changeName()">改变属性值</button>
+        </div>`,
+    // data写成函数的形式，是因为，组件可能被多处调用，
+    // 写成data的形式可能会造成数据共享而影响所有该组件的调用
+    // 而写成函数则每一个调用都有自己的数据
+    // data: function() 等价于 data()
+    // 组件中均写为函数形式
+    data: function () {
+        return {
+            name: "属性1"
+        }
+    },
+    methods: {
+        changeName: function() {
+            this.name = "属性2";
+        }
+    }
 });
 
 /* new Vue({
