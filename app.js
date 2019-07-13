bcc2625e4469e8f63b6d1adf0130ef2d9ef16557
mym <mym9413@outlook.com>
2019-07-13 14:41:14 +0800

    实例化多个vue对象

--- app.js
+++ app.js
@@ -1,30 +1,34 @@
+// 多个vue实例化对象
+
 var one = new Vue({
     el: '#vue-app-one',
     data: {
-      title: 'Vue App One'
+        title: 'Vue App One'
     },
     computed: {
-      greet: function(){
-        return 'Hello, from app one :)';
-      }
+        greet: function () {
+            return 'Hello, from app one :)';
+        }
     }
 });
 
 var two = new Vue({
     el: '#vue-app-two',
     data: {
-      title: 'Vue App Two'
+        title: 'Vue App Two'
     },
     computed: {
-      greet: function(){
-        return 'Yo dudes, this is app 2 speaking to ya';
-      }
+        greet: function () {
+            return 'Yo dudes, this is app 2 speaking to ya';
+        }
     },
     methods: {
-      changeTitle: function(){
-        one.title = 'Title Changed';
-      }
+        changeTitle: function () {
+            // 调用另一个vue对象，形式如下
+            one.title = 'Title Changed';
+        }
     }
 });
 
+// 调用vue对象的属性
 two.title = 'Changed from outside';
