3165e214cdd5e51f24623491f9caca3476b348d2
mym <mym9413@outlook.com>
2019-07-13 10:52:52 +0800

    使用ref和v-model实现双向数据绑定

--- app.js
+++ app.js
@@ -5,11 +5,16 @@ new Vue({
         age: ''
     },
     methods: {
-        logName: function(){
+        // 双向数据绑定：方式一 使用ref
+        logName: function () {
+            // 在input标签中加了ref属性，使用this.$refs.name获取input框
+            // 再使用.value获取input的value属性的值
+            this.name = this.$refs.name.value;
             console.log('you entered your name');
         },
-        logAge: function(){
+        // 双向数据绑定：方式二 使用v-model
+        logAge: function () {
             console.log('you entered your age');
         }
-    }
+    }   
 });
